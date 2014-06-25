9726661f3bc903dc95d4b27a857f4abb62e325f0
Ivan Safrin <ivansafrin@gmail.com>
2014-06-25 01:20:09 -0400

    Added more tutorial content

--- learn/learn_header.php
+++ learn/learn_header.php
@@ -9,18 +9,20 @@
 	<h4>Basics</h4>
 	<div class="list-group">
             <a href="/learn" class="list-group-item">Introduction</a>
-            <a href="/learn/scripting" class="list-group-item">A note on Lua and C++</a>
+            <a href="/learn/scripting" class="list-group-item">Lua scripting</a>
             <a href="/learn/structure" class="list-group-item">Basic app structure</a>
          </div>
 
 	<h4>Graphics</h4>
 	<div class="list-group">
             <a href="/learn/scenes" class="list-group-item">Scenes and entities</a>
-            <a href="/learn/primitives" class="list-group-item">Rendering primitives</a>
+            <a href="/learn/cameras" class="list-group-item">Cameras and resolutions</a>
+            <a href="/learn/primitives" class="list-group-item">Images and primitives</a>
             <a href="/learn/text" class="list-group-item">Rendering text</a>
             <a href="/learn/meshes" class="list-group-item">Rendering meshes</a>
             <a href="/learn/skeletons" class="list-group-item">Skeletal animation</a>
-            <a href="/learn/materials" class="list-group-item">Textures, materials and lights</a>
+            <a href="/learn/textures" class="list-group-item">Textures and images</a>
+            <a href="/learn/materials" class="list-group-item">Materials and lights</a>
             <a href="/learn/post" class="list-group-item">Post processing</a>
             <a href="/learn/particles" class="list-group-item">Particle systems</a>
             <a href="/learn/render_textures" class="list-group-item">Rendering to texture and virtual scenes</a>
--- site/header.php
+++ site/header.php
@@ -7,6 +7,18 @@
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="/css/main.css">
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-309912-5']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 <div class="navbar navbar-inverse navbar-static-top" role="navigation">
